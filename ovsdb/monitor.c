c899f10dd0bff25825a52aa603652730a6da9d74
Liran Schour <lirans@il.ibm.com>
2016-01-28 13:28:39 +0000

    ovsdb: allow unmonitored columns in condition evaluation
    This commit allows to add unmonitored columns to a monitored table
    due to condition update.
    It will be used to evaluate conditions on unmonitored columns.
    Update notification includes only monitored columns.
    Due to the limited number of columns, we do not remove unused unmonitored
    columns on condition update for code simplicity.
    
    Signed-off-by: Liran Schour <lirans@il.ibm.com>

--- ovsdb/monitor.c
+++ ovsdb/monitor.c
@@ -75,6 +75,7 @@ struct jsonrpc_monitor_node {
 struct ovsdb_monitor_column {
     const struct ovsdb_column *column;
     enum ovsdb_monitor_selection select;
+    bool monitored;
 };
 
 /* A row that has changed in a monitored table. */
@@ -115,6 +116,8 @@ struct ovsdb_monitor_table {
     /* Columns being monitored. */
     struct ovsdb_monitor_column *columns;
     size_t n_columns;
+    size_t n_monitored_columns;
+    size_t allocated_columns;
 
     /* Columns in ovsdb_monitor_row have different indexes then in
      * ovsdb_row. This field maps between column->index to the index in the
@@ -201,6 +204,11 @@ compare_ovsdb_monitor_column(const void *a_, const void *b_)
 {
     const struct ovsdb_monitor_column *a = a_;
     const struct ovsdb_monitor_column *b = b_;
+
+    /* put all monitored columns at the begining */
+    if (a->monitored != b->monitored) {
+        return a->monitored ? -1 : 1;
+    }
 
     return a->column < b->column ? -1 : a->column > b->column;
 }
@@ -377,15 +385,23 @@ ovsdb_monitor_add_column(struct ovsdb_monitor *dbmon,
                          const struct ovsdb_table *table,
                          const struct ovsdb_column *column,
                          enum ovsdb_monitor_selection select,
-                         size_t *allocated_columns)
+                         bool monitored)
 {
     struct ovsdb_monitor_table *mt;
     struct ovsdb_monitor_column *c;
 
     mt = shash_find_data(&dbmon->tables, table->schema->name);
 
-    if (mt->n_columns >= *allocated_columns) {
-        mt->columns = x2nrealloc(mt->columns, allocated_columns,
+    /* Check duplication only for unmonitored columns.
+     * Duplicated monitored columns will cause cancelation of monitor request
+     * on ovsdb_monitor_table_check_duplicates() call */
+    if (!monitored && mt->columns_index_map[column->index] != -1) {
+        /* column exist */
+        return;
+    }
+
+    if (mt->n_columns >= mt->allocated_columns) {
+        mt->columns = x2nrealloc(mt->columns, &mt->allocated_columns,
                                  sizeof *mt->columns);
     }
 
@@ -394,6 +410,10 @@ ovsdb_monitor_add_column(struct ovsdb_monitor *dbmon,
     c = &mt->columns[mt->n_columns++];
     c->column = column;
     c->select = select;
+    c->monitored = monitored;
+    if (monitored) {
+        mt->n_monitored_columns++;
+    }
 }
 
 /* Check for duplicated column names. Return the first
@@ -573,10 +593,10 @@ ovsdb_monitor_compose_row_update(
         new_json = json_object_create();
         json_object_put(row_json, "new", new_json);
     }
-    for (i = 0; i < mt->n_columns; i++) {
+    for (i = 0; i < mt->n_monitored_columns; i++) {
         const struct ovsdb_monitor_column *c = &mt->columns[i];
 
-        if (!(type & c->select)) {
+        if (!c->monitored || !(type & c->select))  {
             /* We don't care about this type of change for this
              * particular column (but we will care about it for some
              * other column). */
@@ -630,10 +650,10 @@ ovsdb_monitor_compose_row_update2(
         diff_json = json_object_create();
         const char *op;
 
-        for (i = 0; i < mt->n_columns; i++) {
+        for (i = 0; i < mt->n_monitored_columns; i++) {
             const struct ovsdb_monitor_column *c = &mt->columns[i];
 
-            if (!(type & c->select)) {
+            if (!c->monitored || !(type & c->select))  {
                 /* We don't care about this type of change for this
                  * particular column (but we will care about it for some
                  * other column). */
@@ -1023,19 +1043,21 @@ ovsdb_monitor_table_equal(const struct ovsdb_monitor_table *a,
 {
     size_t i;
 
+    ovs_assert(b->n_columns == b->n_monitored_columns);
+
     if ((a->table != b->table) ||
         (a->select != b->select) ||
-        (a->n_columns != b->n_columns)) {
+        (a->n_monitored_columns != b->n_monitored_columns)) {
         return false;
     }
 
-    for (i = 0; i < a->n_columns; i++) {
+    /* Compare only monitored columns that must be sorted already */
+    for (i = 0; i < a->n_monitored_columns; i++) {
         if ((a->columns[i].column != b->columns[i].column) ||
             (a->columns[i].select != b->columns[i].select)) {
             return false;
         }
     }
-
     return true;
 }
 

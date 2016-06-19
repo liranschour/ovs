021b91f613977fdd35d635295420ff73f87d6920
Liran Schour <lirans@il.ibm.com>
2016-06-19 14:33:32 +0300

    ovn: implementation of conditional monitoring usage
    Conditional monitor of: Port_Binding, Logical_Flow, Multicast_Group
    MAC_Binding tables. As a result ovn-controller will be notified only about
    records belongs to a datapath that is being served by this hypervisor.
    
    Performance evaluation:
    OVN is the main candidate for conditional monitoring usage. It is clear that
    conditional monitoring reduces computation on the ovn-controller (client) side
    due to the reduced size of flow tables and update messages. Performance
    evaluation shows up to 75% computation reduction on the ovn-controller side.
    However, performance evaluation shows also a reduction in computation on the SB
    ovsdb-server side proportional to the degree that each logical network is
    spread over physical hosts in the DC. Evaluation shows that in a realistic
    scenarios there is a computation reduction also in the server side.
    
    Evaluation on simulated environment of 50 hosts and 1000 logical ports shows
    the following results (cycles #):
    
    LN spread over # hosts|    master    | patch        | change
    -------------------------------------------------------------
                1         | 24597200127  | 24339235374  |  1.0%
                6         | 23788521572  | 19145229352  | 19.5%
               12         | 23886405758  | 17913143176  | 25.0%
               18         | 25812686279  | 23675094540  |  8.2%
               24         | 28414671499  | 24770202308  | 12.8%
               30         | 31487218890  | 28397543436  |  9.8%
               36         | 36116993930  | 34105388739  |  5.5%
               42         | 37898342465  | 38647139083  | -1.9%
               48         | 41637996229  | 41846616306  | -0.5%
               50         | 41679995357  | 43455565977  | -4.2%
    
    TODO:
    - If performance evaluation will show the need, add a tenant-id
      (connectivity-id) column in the datapath_binding table to reduce the number
      of hops that is required to retrieve all datapaths.
    
    Signed-off-by: Liran Schour <lirans@il.ibm.com>

---
+++ ovn/controller/filter.h
@@ -0,0 +1,46 @@
+/* Copyright (c) 2015 Nicira, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OVN_FILTER_H
+#define OVN_FILTER_H 1
+
+/* Database client filtering
+ * -------------------------
+ *
+ * By default the OVSDB IDL replicates the entire contents of each table.  For
+ * some tables, however, ovn-controller only needs some rows.  For example, in
+ * the Logical_Flow table, it only needs the rows for logical datapaths that
+ * are in use directly or indirectly on this hypervisor.  These functions aid
+ * in limiting the rows that the IDL replicates.
+ */
+
+#include <stdint.h>
+
+struct controller_ctx;
+struct ovsdb_idl;
+struct sbrec_datapath_binding;
+struct lport_index;
+
+void filter_init(struct ovsdb_idl *);
+void filter_clear(struct ovsdb_idl *);
+void filter_mark_unused(void);
+void filter_remove_unused_lports(struct controller_ctx *,
+                                 const struct lport_index *);
+void filter_lport(struct controller_ctx *, const char *lport_name);
+void filter_datapath(struct controller_ctx *,
+                     const struct sbrec_datapath_binding *);
+void unfilter_datapath(struct controller_ctx *, int64_t tunnel_key);
+
+#endif /* ovn/controller/filter.h */
